68c4b082f2f8a275d4b948c4cc53fa98ae6a3b38
Paivy <paivyeshirera@gmail.com>
2022-05-02 07:01:40 +0300

    add the Articles class to models

---
+++ app/models.py
@@ -0,0 +1,15 @@
+class Articles:
+    """
+    This is the Articles class that defines Articles Objects
+    """
+    def __init__(self,source,title,author,description,urlToimage,content,publishedAt):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToimage = urlToimage
+        self.content = content
+        self.publishedAt = publishedAt
+
+
+

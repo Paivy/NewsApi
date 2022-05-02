e063503e5b1cb6905567c2340e8548e585b2afc9
Paivy <paivyeshirera@gmail.com>
2022-05-02 07:27:04 +0300

    Renamed files correctly

--- app/models.py
+++ app/models.py
@@ -12,4 +12,15 @@ class Articles:
         self.publishedAt = publishedAt
 
 
-
+# class Sources:
+#     '''
+#     Sources class that defines each source object
+#     '''
+#     def __init__(self,id,name,description,url,category,language,country):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.url = url
+#         self.category = category
+#         self.language = language
+#         self.country = country

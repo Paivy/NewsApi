1cf03053052638441cc624fc1f5f710e5cfaf150
Paivy <paivyeshirera@gmail.com>
2022-05-03 11:51:14 +0300

    modified the file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,22 @@
-# from flask import Flask
-# # Initializing application
+from ensurepip import bootstrap
+from flask import Flask
+from app.requests import configure_request
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-# app= Flask(__name__)
+bootstrap = Bootstrap
+
+def create_app(config_name):
+    app = flask(__name__)
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    #Registering the blueprints
+    from.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    #setting config
+    configure_request(app)
+
+    return app

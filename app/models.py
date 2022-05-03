c9eeac6b89fa72e6555df2ea45019e31219383c4
Paivy <paivyeshirera@gmail.com>
2022-05-03 08:59:43 +0300

    modified the class articles to news

--- app/models.py
+++ app/models.py
@@ -1,10 +1,9 @@
-class Articles:
+class News:
     """
-    This is the Articles class that defines Articles Objects
+    This is the News class that defines News Objects
     """
-    def __init__(self,source,title,author,description,urlToimage,content,publishedAt):
-        self.source = source
-        self.author = author
+    def __init__(self,title,description,urlToimage,content,publishedAt):
+        
         self.title = title
         self.description = description
         self.urlToimage = urlToimage
@@ -16,11 +15,9 @@ class Articles:
 #     '''
 #     Sources class that defines each source object
 #     '''
-#     def __init__(self,id,name,description,url,category,language,country):
-#         self.id = id
+#     def __init__(self,name,description,url):
+#        
 #         self.name = name
 #         self.description = description
 #         self.url = url
-#         self.category = category
-#         self.language = language
-#         self.country = country
+#        

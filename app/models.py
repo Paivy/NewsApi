083c962deb0d7ba72b697da56670f4f35535a1b2
Paivy <paivyeshirera@gmail.com>
2022-05-04 07:39:51 +0300

    added the sources class

--- app/models.py
+++ app/models.py
@@ -11,13 +11,15 @@ class News:
         self.publishedAt = publishedAt
 
 
-# class Sources:
-#     '''
-#     Sources class that defines each source object
-#     '''
-#     def __init__(self,name,description,url):
-#        
-#         self.name = name
-#         self.description = description
-#         self.url = url
-#        
+class Sources:
+    '''
+    Sources class that defines each source object
+    '''
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country

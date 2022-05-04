07a9374fa571c19d15c7ae0f2795ea78fd2d9f72
Paivy <paivyeshirera@gmail.com>
2022-05-04 15:12:08 +0300

    updated my models structure

--- app/models.py
+++ app/models.py
@@ -11,15 +11,13 @@ class News:
         self.publishedAt = publishedAt
 
 
-class Sources:
-    '''
-    Sources class that defines each source object
-    '''
-    def __init__(self,id,name,description,url,category,language,country):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-        self.language = language
-        self.country = country
+# class Sources:
+#     '''
+#     Sources class that defines each source object
+#     '''
+#     def __init__(self,name,description,url):
+       
+#         self.name = name
+#         self.description = description
+#         self.url = url
+       

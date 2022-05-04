69c2b8b9f319f41b3651880efc01b8baff0ca155
Paivy <paivyeshirera@gmail.com>
2022-05-04 15:24:58 +0300

    added the api keys

--- config.py
+++ config.py
@@ -5,8 +5,15 @@ class Config:
     """
     General configuration parent class
     """
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey= d19196c92d7e4608b4948b64b292c88b'
-    NEWS_API_KEY  = os.environ.get('d19196c92d7e4608b4948b64b292c88b')
+    # NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey= d19196c92d7e4608b4948b64b292c88b'
+    # NEWS_API_KEY  = os.environ.get('d19196c92d7e4608b4948b64b292c88b')
+
+    NEWS_API_BASE_URL =  'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_ARTICLES_APL_URL='https://newsapi.org/v2/everything?q={}&apiKey={}'
+    SOURCE_ARTICLES_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}' 
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class  ProdConfig(Config):
